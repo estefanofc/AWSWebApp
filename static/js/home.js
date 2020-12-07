3e8552a10048de7c3a7059ca29cd30839c812ecb
Estefano Felipa <estefano_fc@hotmail.com>
2020-12-07 00:37:25 -0800

    added alerts for clear and load

--- static/js/home.js
+++ static/js/home.js
@@ -1,1 +1,7 @@
+function alertLoad() {
+    alert('Loading data from website to S3 Bucket and Database');
+}
 
+function alertClear() {
+    alert('Clearing data from website to S3 Bucket and Database');
+}
